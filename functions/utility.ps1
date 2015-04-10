c18748883fa4420afac430e3373cc8947a713a93
Robert Labrie <robert.labrie@gmail.com>
2015-04-10 13:27:41 -0400

    encoded json depth helps with nesting

--- functions/utility.ps1
+++ functions/utility.ps1
@@ -18,7 +18,7 @@ Function ConvertTo-EncodedJSON {
         ]
         $inputObject
     )
-    $json = $inputObject | ConvertTo-Json | Out-String
+    $json = $inputObject | ConvertTo-Json -Depth 32 | Out-String
     $enc = [System.Web.HttpUtility]::UrlEncode($json)
     return $enc
 }
